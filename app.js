8058ef5d69db30a3856a9a1aec22c7155041ba32
Eva Panizo <panizo.eva@gmail.com>
2018-11-08 11:10:58 +0100

    Fist commit after validation

--- app.js
+++ app.js
@@ -29,6 +29,7 @@ app.use(cors({
   credentials: true,
   origin: [process.env.PUBLIC_DOMAIN]
 }));
+
 // app.use((req, res, next) => {
 //   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
 //   res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,OPTIONS,DELETE');
