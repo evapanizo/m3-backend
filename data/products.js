ef830c68047d005006cf5508d625cdb109c8c1bf
Eva Panizo <panizo.eva@gmail.com>
2018-11-14 11:29:40 +0100

    seed data update

--- data/products.js
+++ data/products.js
@@ -1,61 +1,121 @@
 const products = [
   {
-    name: 'Carrots',
-    image: '',
+    name: 'Carrot',
+    image: 'https://bit.ly/2FiKD3u',
     stock: 2000,
-    description: ''
+    description: 'Carrots contain vitamin A, antioxidants, and other nutrients. Evidence suggests that eating more antioxidant-rich fruits and vegetables, such as carrots, can help reduce the risks of cancer and cardiovascular disease. Carrots are also rich in vitamins, minerals, and fiber.'
   },
   {
-    name: 'Potatoes',
-    image: '',
+    name: 'Potato',
+    image: 'https://bit.ly/2DCebHJ',
     stock: 1000,
     description: ''
   },
   {
     name: 'Sweet Corn',
-    image: '',
+    image: 'https://bit.ly/2QIk5dm',
     stock: 1,
     description: ''
   },
   {
     name: 'Green Bean',
-    image: '',
+    image: 'https://bit.ly/2TalB9K',
     stock: 3000,
     description: ''
   },
   {
     name: 'Brocoli',
-    image: '',
+    image: 'https://bit.ly/2PX52zk',
     stock: 3,
     description: ''
   },
   {
     name: 'Tomato',
-    image: '',
+    image: 'https://bit.ly/2PVhqQp',
     stock: 1500,
     description: ''
   },
   {
     name: 'Garlic',
-    image: '',
+    image: 'https://bit.ly/2PVhq2R',
     stock: 700,
     description: ''
   },
   {
     name: 'Cucumber',
-    image: '',
+    image: 'https://bit.ly/2RQrTtN',
     stock: 500,
     description: ''
   },
   {
     name: 'Spinach',
-    image: '',
+    image: 'https://bit.ly/2QFIpfT',
     stock: 1000,
     description: ''
   },
   {
     name: 'Onion',
-    image: '',
+    image: 'https://bit.ly/2PU3HJG',
+    stock: 5,
+    description: ''
+  },
+  {
+    name: 'Apple',
+    image: 'https://bit.ly/2RNrvw6',
+    stock: 2000,
+    description: ''
+  },
+  {
+    name: 'Banana',
+    image: 'https://bit.ly/2Ppbfol',
+    stock: 1000,
+    description: ''
+  },
+  {
+    name: 'Pear',
+    image: 'https://bit.ly/2FiSXQL',
+    stock: 1,
+    description: ''
+  },
+  {
+    name: 'Peach',
+    image: 'https://bit.ly/2QBSGJT',
+    stock: 3000,
+    description: ''
+  },
+  {
+    name: 'Orange',
+    image: 'https://bit.ly/2za7zN8',
+    stock: 3,
+    description: ''
+  },
+  {
+    name: 'Kiwi',
+    image: 'https://bit.ly/2T8qrV0',
+    stock: 1500,
+    description: ''
+  },
+  {
+    name: 'Apricot',
+    image: 'https://bit.ly/2zQDRwc',
+    stock: 700,
+    description: ''
+  },
+  {
+    name: 'Blueberries',
+    image: 'https://bit.ly/2DliPJq',
+    stock: 500,
+    description: ''
+  },
+  {
+    name: 'Grapes',
+    image: 'https://bit.ly/2OIkgn6',
+    stock: 1000,
+    description: ''
+  },
+  {
+    name: 'Dragon Fruit',
+    image: 'https://bit.ly/2K03y1M',
     stock: 5,
     description: ''
   }
